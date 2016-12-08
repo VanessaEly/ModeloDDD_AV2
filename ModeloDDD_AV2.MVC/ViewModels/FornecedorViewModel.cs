54c8e613afeeaf7b956a33430e42d5d231a55157
Guilherme d' Avila Vertein <vertein.dev@gmail.com>
2016-12-07 22:48:11 -0200

    Adicionado MaxLength para os atributos da classe de acordo com os requisitos do projeto.
    Adicionado Expressão Regular para os atributos inscrição municipal e CNPJ.

--- ModeloDDD_AV2.MVC/ViewModels/FornecedorViewModel.cs
+++ ModeloDDD_AV2.MVC/ViewModels/FornecedorViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace ModeloDDD_AV2.MVC.ViewModels
 {
@@ -12,13 +9,19 @@ namespace ModeloDDD_AV2.MVC.ViewModels
         [Key]
         public int FornecedorId { get; set; }
 
-        [Required(ErrorMessage = "Preencha o campo Cnpj.")]
+        [Required(ErrorMessage = "Preencha o campo CNPJ.")]
+        [MaxLength(14, ErrorMessage = "Máximo de {0} caracteres.")]
+        [RegularExpression(@"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)", ErrorMessage = "O CNPJ deverá estar no formato 00.000.000/0000-00")]
         public string Cnpj { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Razao Social.")]
+        [MaxLength(200, ErrorMessage = "Máximo de {0} caracteres.")]
         public string RazaoSocial { get; set; }
 
         [Required(ErrorMessage = "Preencha o campo Inscrição Municipal.")]
+        [MinLength(8, ErrorMessage = "Mínimo de {0} caracteres.")]
+        [MaxLength(8, ErrorMessage = "Máximo de {0} caracteres.")]
+        [RegularExpression(@"(^(\d{1}.\d{3}.\d{3} -\d{1})|(\d{8})$)", ErrorMessage = "A Inscrição Municipal deverá estar no formato 0.000.000-0")]
         public string InscricaoMunicipal { get; set; }
 
         [DataType(DataType.Currency)]
