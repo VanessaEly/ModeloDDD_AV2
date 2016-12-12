b74a41b9d3ffc60acb6c8b24a9efb204a70f18e8
VerteinGDV <vertein.dev@gmail.com>
2016-12-12 02:02:14 -0200

    Adicionado o metodo CalcularMulta na Interface IAutoDeInfracaoAppServiceBase e na Classe AutoDeInfracaoAppServiceBase

--- ModeloDDD_AV2.Application/AutoDeInfracaoAppServiceBase.cs
+++ ModeloDDD_AV2.Application/AutoDeInfracaoAppServiceBase.cs
@@ -1,11 +1,6 @@
 ﻿using ModeloDDD_AV2.Application.Interface;
 using ModeloDDD_AV2.Domain.Entities;
 using ModeloDDD_AV2.Domain.Interfaces.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModeloDDD_AV2.Application
 {
@@ -16,6 +11,11 @@ namespace ModeloDDD_AV2.Application
         public AutoDeInfracaoAppService(IAutoDeInfracaoService AutoDeInfracaoService) : base(AutoDeInfracaoService)
         {
             _AutoDeInfracaoService = AutoDeInfracaoService;
+        }
+
+        public decimal CalcularMulta(AutoDeInfracao autoDeInfracao, Fornecedor fornecedor)
+        {
+            return autoDeInfracao.CalculoMulta(autoDeInfracao, fornecedor);
         }
     }
 }
--- ModeloDDD_AV2.Application/Interface/IAutoDeInfracaoAppServiceBase.cs
+++ ModeloDDD_AV2.Application/Interface/IAutoDeInfracaoAppServiceBase.cs
@@ -1,14 +1,9 @@
 ﻿using ModeloDDD_AV2.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModeloDDD_AV2.Application.Interface
 {
     public interface IAutoDeInfracaoAppService : IAppServiceBase<AutoDeInfracao>
     {
-
+        decimal CalcularMulta(AutoDeInfracao autoDeInfracao, Fornecedor fornecedor);
     }
 }
